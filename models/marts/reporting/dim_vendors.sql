fd045282f40e35ff462550aad0c90ad7d3db61c4
Jared Chuang <jared.chuang128@gmail.com>
2026-03-05 21:38:56 +0000

    update names

--- models/marts/reporting/dim_vendors.sql
+++ models/marts/reporting/dim_vendors.sql
@@ -1,12 +1,15 @@
-with trips_unioned as (
-    select * from {{ ref('int_trips_unioned') }}
+-- Dimension table for taxi technology vendors
+-- Small static dimension defining vendor codes and their company names
+
+with trips as (
+    select * from {{ ref('fct_trips') }}
 ),
 
 vendors as (
-    select
-        distinct vendor_id,
+    select distinct
+        vendor_id,
         {{ get_vendor_data('vendor_id') }} as vendor_name
-    from trips_unioned
+    from trips
 )
 
 select * from vendors
